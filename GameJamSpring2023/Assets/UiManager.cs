0d75e5b0276da2e79087ff36465610c8fae50213
Kai Schellin <47905053+KaiNet-X@users.noreply.github.com>
2023-03-24 22:39:10 -0500

    Commit

--- GameJamSpring2023/Assets/UiManager.cs
+++ GameJamSpring2023/Assets/UiManager.cs
@@ -2,8 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 public class UiManager : MonoBehaviour
 {
+    public static float MasterVol = 1f;
+    public static float SfxVol = 1f;
+    public static float MusicVol = 1f;
+
+    [SerializeField] public Slider MasterVolSlider;
+    [SerializeField] public Slider SfxVolSlider;
+    [SerializeField] public Slider MusicVolSlider;
+
     public void StartGame() => SceneManager.LoadScene(1);
+
+    public void UpdateMasterVol() => MasterVol = MasterVolSlider.value;
+    public void UpdateSfxVol() => SfxVol = SfxVolSlider.value * MasterVol;
+    public void UpdateMusicVol() => MusicVol = MusicVolSlider.value * MasterVol;
 }
