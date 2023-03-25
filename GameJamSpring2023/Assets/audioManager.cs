94f9f5df66d1c0c3f9a2f5bab8a415bda9c2f7ba
TheShauceGod <bdiaz9@iastate.edu>
2023-03-25 12:30:32 -0500

    Audio Manananananager

--- GameJamSpring2023/Assets/audioManager.cs
+++ GameJamSpring2023/Assets/audioManager.cs
@@ -1,17 +1,27 @@
 using UnityEngine.Audio;
+using System;
 using UnityEngine;
 
 public class audioManager : MonoBehaviour
 {
+    public Sound[] sounds;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Play (String name)
     {
-        
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        s.source.Play();
     }
 }
